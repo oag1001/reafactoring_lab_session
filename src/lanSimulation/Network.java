c0d578e77597e65d92ff81d3d8ee02640b491467
Víctor Pascual Rodríguez <93974380+vpr1004@users.noreply.github.com>
2022-05-18 16:30:58 +0200

    refactorización mover metodo

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -219,7 +219,7 @@ public class Network {
 				report.write("\tNode '");
 				report.write(currentNode.name_);
 				report.write("' accepts broadcase packet.\n");
-				logging(report, currentNode);
+				currentNode.logging(report, this);
 			} catch (IOException exc) {
 				// just ignore
 			}
@@ -278,7 +278,7 @@ public class Network {
 		startNode = (Node) workstations_.get(workstation);
 
 		try {
-			logging(report, startNode);
+			startNode.logging(report, this);
 		} catch (IOException exc) {
 			// just ignore
 		}
@@ -286,7 +286,7 @@ public class Network {
 		currentNode = startNode.nextNode_;
 		while ((!packet.destination_.equals(currentNode.name_)) & (!packet.origin_.equals(currentNode.name_))) {
 			try {
-				logging(report, currentNode);
+				currentNode.logging(report, this);
 			} catch (IOException exc) {
 				// just ignore
 			}
@@ -296,7 +296,7 @@ public class Network {
 		;
 
 		if (packet.destination_.equals(currentNode.name_)) {
-			result = printDocument(currentNode, packet, report);
+			result = packet.printDocument(currentNode, this, report);
 		} else {
 			try {
 				report.write(">>> Destinition not found, print job cancelled.\n\n");
@@ -311,69 +311,7 @@ public class Network {
 		return result;
 	}
 
-	private void logging(Writer report, Node currentNode) throws IOException {
-		report.write("\tNode '");
-		report.write(currentNode.name_);
-		report.write("' passes packet on.\n");
-		report.flush();
-	}
-
-	private boolean printDocument(Node printer, Packet document, Writer report) {
-		String author = "Unknown";
-		String title = "Untitled";
-		int startPos = 0, endPos = 0;
-
-		if (printer.type_ == Node.PRINTER) {
-			try {
-				if (document.message_.startsWith("!PS")) {
-					startPos = document.message_.indexOf("author:");
-					if (startPos >= 0) {
-						endPos = document.message_.indexOf(".", startPos + 7);
-						if (endPos < 0) {
-							endPos = document.message_.length();
-						}
-						;
-						author = document.message_.substring(startPos + 7, endPos);
-					}
-					;
-					startPos = document.message_.indexOf("title:");
-					if (startPos >= 0) {
-						endPos = document.message_.indexOf(".", startPos + 6);
-						if (endPos < 0) {
-							endPos = document.message_.length();
-						}
-						;
-						title = document.message_.substring(startPos + 6, endPos);
-					}
-					;
-					accounting(report, author, title);
-				} else {
-					title = "ASCII DOCUMENT";
-					if (document.message_.length() >= 16) {
-						author = document.message_.substring(8, 16);
-					}
-					;
-					accounting(report, author, title);
-				}
-				;
-			} catch (IOException exc) {
-				// just ignore
-			}
-			;
-			return true;
-		} else {
-			try {
-				report.write(">>> Destinition is not a printer, print job cancelled.\n\n");
-				report.flush();
-			} catch (IOException exc) {
-				// just ignore
-			}
-			;
-			return false;
-		}
-	}
-
-	private void accounting(Writer report, String author, String title) throws IOException {
+	public void accounting(Writer report, String author, String title) throws IOException {
 		report.write("\tAccounting -- author = '");
 		report.write(author);
 		report.write("' -- title = '");
--- src/lanSimulation/internals/Packet.java
+++ src/lanSimulation/internals/Packet.java
@@ -19,6 +19,11 @@
  */
 package lanSimulation.internals;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import lanSimulation.Network;
+
 /**
  * A <em>Packet</em> represents a unit of information to be sent over the Local
  * Area Network (LAN).
@@ -53,6 +58,61 @@ public class Packet {
 		message_ = message;
 		origin_ = origin;
 		destination_ = destination;
+	}
+
+	public boolean printDocument(Node printer, Network network, Writer report) {
+		String author = "Unknown";
+		String title = "Untitled";
+		int startPos = 0, endPos = 0;
+	
+		if (printer.type_ == Node.PRINTER) {
+			try {
+				if (message_.startsWith("!PS")) {
+					startPos = message_.indexOf("author:");
+					if (startPos >= 0) {
+						endPos = message_.indexOf(".", startPos + 7);
+						if (endPos < 0) {
+							endPos = message_.length();
+						}
+						;
+						author = message_.substring(startPos + 7, endPos);
+					}
+					;
+					startPos = message_.indexOf("title:");
+					if (startPos >= 0) {
+						endPos = message_.indexOf(".", startPos + 6);
+						if (endPos < 0) {
+							endPos = message_.length();
+						}
+						;
+						title = message_.substring(startPos + 6, endPos);
+					}
+					;
+					network.accounting(report, author, title);
+				} else {
+					title = "ASCII DOCUMENT";
+					if (message_.length() >= 16) {
+						author = message_.substring(8, 16);
+					}
+					;
+					network.accounting(report, author, title);
+				}
+				;
+			} catch (IOException exc) {
+				// just ignore
+			}
+			;
+			return true;
+		} else {
+			try {
+				report.write(">>> Destinition is not a printer, print job cancelled.\n\n");
+				report.flush();
+			} catch (IOException exc) {
+				// just ignore
+			}
+			;
+			return false;
+		}
 	}
 
 }
